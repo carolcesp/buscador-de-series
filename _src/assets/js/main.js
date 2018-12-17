b5eacd6cc783e4379e70773ee06ef230be006bf2
carolcesp <carolinacespedes90@gmail.com>
2018-12-17 20:09:06 +0100

    localstorage example

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -13,35 +13,36 @@ const api = 'http://api.tvmaze.com/search/shows?q=';
 const handleShow = () => {
 //busco el valor de mi input
   const searchInput = contentInput.value;
-  console.log(searchInput);
-
 	fetch(api + searchInput)
 		.then(response => response.json())
 		.then(data => {
-      console.log(data);
       result(data);
 	});
 }
 const result = (data) => {
-  console.log('result' + data);
+//console.log('result' + data);
+//recorro el array data
   for(let i=0; i < data.length; i++){
-//busco la imagen y si es null le añadimos esta por defecto
+//busco la imagen y si es null le añadimos esta por defecto (https://via.placeholder.com/210x295/cccccc/666666/?text=TV)
       if (data[i].show.image == null){
-        image = 'https://via.placeholder.com/250x300';
+        image = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
 // y sino, me pone la imagen que esta dentro de (image.medium)
       } else {
 		image = data[i].show.image.medium;
     }
 // a mi Ul del html le añado este contenido
-    list.innerHTML +=`<li class="itemLi"><div class="content"><img class="img" src="${image}" alt=""><h2 class="title_content">${data[i].show.name}</h2></div>
-    </li>`;
-
-  }
-favorite();
+    list.innerHTML +=`
+    <li class="itemLi">
+      <div class="content"><img class="img_content" src="${image}" alt="">
+        <h2 class="title_content">${data[i].show.name}</h2>
+      </div>
+    </li>
+    `;}
+//llamo a mi función de favorito
+favourite();
 };
-
-//acceder a los Li con clase (.content)
-function favorite (){
+//acceder a los Li con clase (.itemLi)
+function favourite (){
   let itemLi = document.querySelectorAll('.itemLi');
 //recorro los Li y accedo con click
   for(let i = 0;i < itemLi.length; i++){
@@ -49,17 +50,31 @@ function favorite (){
   itemLi[i].addEventListener('click', addClass);
   }
 }
-
+//añadir clase cuando hago click
 function addClass(e){
 //elemento sobre el que esta el listener.
-    const itemClick = e.currentTarget;
-//a itemClick le añado la clase (itemLi-favorite)
-    itemClick.classList.toggle('itemLi-favorite');
-}
-//LocalStorage
-localStorage.setItem('favorite', 'carol');
-const show = localStorage.getItem('favorite');
-console.log(favorite); //Ana
+  const itemClick = e.currentTarget;
+//a itemClick le añado la clase (itemLi-favourite)
+  itemClick.classList.toggle('itemLi-favourite');
+};
+
+
+//ejemplo localStorage
+
+const  tasks = [
+  {name: 'Recoger setas en el campo', completed: true},
+  {name: 'Comprar pilas', completed: true},
+  {name: 'Poner una lavadora de blancos', completed: true},
+  {name: 'Aprender cómo funcionan los objetos de JavaScript', completed: false}
+];
+
+localStorage.setItem('taks', JSON.stringify(tasks));
+
+const  savedTasks = JSON.parse(localStorage.getItem('taks'));
+console.log(savedTasks.length); //4
+
+
 
 
 button.addEventListener('click', handleShow);
+
